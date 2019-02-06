07090acdd36d964794bf62fc39a456b37f347075
Benjamin Kidd <Benjamin.Kidd@bedegaming.com>
2019-02-06 18:16:00 +0000

    tut 1-4

---
+++ FSharpTestOne/Tut1.fs
@@ -0,0 +1,43 @@
+﻿// Learn more about F# at http://fsharp.org
+module a.Tut1
+(*
+
+let mutable a = 10
+a<-2
+
+let items = [1..5]
+let items2 =List.append items [6]
+items|> ignore
+items2|> ignore
+
+
+let prefix prefixStr baseStr = prefixStr + "," + baseStr
+prefix "hi" "david"
+
+let names = ["David";"Maria";"Alex"]
+let prefixWithHello = prefix "Hello"
+let exclaim s = s+ "!"
+
+names |> Seq.map (prefix "Hello")
+
+names |> Seq.map prefixWithHello |> Seq.map exclaim
+let shout s = s|> Seq.map prefixWithHello |> Seq.map exclaim
+let shout2 = prefixWithHello >> exclaim
+
+names |> shout |> Seq.sort
+names |> Seq.map shout2
+
+let hellos = 
+    names 
+    |> Seq.map (fun x -> printfn "Mapped over %s" x; shout2 x)
+    |> Seq.sort
+    |> Seq.iter (printfn "%s")
+
+hellos
+
+*)
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ FSharpTestOne/Tut2.fs
@@ -0,0 +1,39 @@
+﻿module a.Tut2
+
+type Details = 
+    {Name: string
+     Description: string}
+
+type Item ={
+    Details:Details}
+
+   
+type Exit =
+    | PassableExit of Details * destination: Room
+    | LockedExit of Details * key: Item
+    | NoExit of Details option
+
+
+and Exits ={
+    North: Exit 
+    South: Exit
+    East: Exit
+    West: Exit}
+
+and Room ={
+   Details: Details 
+   Items: Item list
+   Exits: Exits}
+
+
+let firstRoom ={
+    Details = {Name = "First Room"; Description = "You're standing in a room"};
+    Items = [];
+    Exits ={
+        North = NoExit(None);
+        South= NoExit(None);
+        East= NoExit(None);
+        West = NoExit(None)}}
+
+
+firstRoom
